a230676dc1d49d4a62a9fe03ca1cb3a49723bb7e
The-Repo-Club <wayne6324@gmail.com>
2023-03-10 15:59:20 +0000

    Nvim

--- nvim/.config/nvim/lua/user/trim.lua
+++ nvim/.config/nvim/lua/user/trim.lua
@@ -6,7 +6,7 @@ end
 trim.setup({
 	-- if you want to ignore markdown file.
 	-- you can specify filetypes.
-	disable = { "markdown" },
+	ft_blocklist = { "markdown" },
 
 	-- if you want to ignore space of top
 	patterns = {
