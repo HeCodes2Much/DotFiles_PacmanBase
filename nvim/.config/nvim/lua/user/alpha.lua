9f8fa68db069fe811ce35d1057e59d2991bc0350
The-Repo-Club <wayne6324@gmail.com>
2023-07-27 19:25:49 +0100

    Update

--- nvim/.config/nvim/lua/user/alpha.lua
+++ nvim/.config/nvim/lua/user/alpha.lua
@@ -16,13 +16,13 @@ dashboard.section.header.val = {
 	"     [ Think LunaVim   Author:The-Repo-Club ]     ",
 }
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("f", "󰈞  Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
 	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("r", "󰄉  Recently used files", ":Telescope oldfiles <CR>"),
+	dashboard.button("t", "󰊄  Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("c", "  Configuration", ":EditConfiguration <CR>"),
-	dashboard.button("q", "  Quit LunaVim", ":qa <CR>"),
+	dashboard.button("q", "󰅚  Quit LunaVim", ":qa <CR>"),
 }
 
 dashboard.section.footer.val = {
