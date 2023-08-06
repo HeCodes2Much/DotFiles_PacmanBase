0816dcd48221dbb9d4c77727e66cacd326dd4c1b
The-Repo-Club <wayne6324@gmail.com>
2023-08-06 12:34:05 +0100

    Fix repobar

--- repowm/.config/repowm/autostart.sh
+++ repowm/.config/repowm/autostart.sh
@@ -2,17 +2,17 @@
 #-*-coding:utf-8 -*-
 #Auto updated?
 #   Yes
-#File :
+#File:
 #   autostart.sh
-#Author :
+#Author:
 #   The-Repo-Club [wayne6324@gmail.com]
-#Github :
+#Github:
 #   https://github.com/The-Repo-Club/
 #
 #Created:
 #   Mon 22 November 2021, 03:15:54 PM [GMT]
-#Last edited:
-#   Fri 08 July 2022, 07:18:26 AM [GMT+1]
+#Modified:
+#   Sun 06 August 2023, 11:46:15 AM [GMT+1]
 #
 #Description:
 #   <Todo>
@@ -21,14 +21,6 @@
 #   <None>
 #
 
-# if command -v autorandr &>/dev/null; then
-#   if ! pgrep -f "autorandr" >/dev/null; then
-#     autorandr --force --load repowm &
-#   else
-#     echo "autorandr already Running"
-#   fi
-# fi
-
 if command -v /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &>/dev/null; then
   if ! pgrep -f "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1" >/dev/null; then
     /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
@@ -36,22 +28,6 @@ if command -v /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &>/dev/n
     echo "polkit-gnome already Running"
   fi
 fi
-
-# if command -v xroot &>/dev/null; then
-#   if ! pgrep -f "xroot" >/dev/null; then
-#     xroot -r xmenu_desktop &
-#   else
-#     echo "xroot already Running"
-#   fi
-# fi
-
-# if command -v clipmenu-daemon &>/dev/null; then
-#   if ! pgrep -f "clipmenu-daemon" >/dev/null; then
-#     clipmenu-daemon &
-#   else
-#     echo "clipmenu-daemon already Running"
-#   fi
-# fi
 
 if ! command -v feh &>/dev/null; then
   echo "feh could not be found"
@@ -70,85 +46,30 @@ if ! command -v picom &>/dev/null; then
   exit
 else
   if ! pgrep -x "picom" >/dev/null; then
-    picom -b --experimental-backend --config $HOME/.config/repowm/picom.conf &
+    picom --config $HOME/.config/repowm/picom.conf &
   else
     echo "picom already Running"
   fi
 fi
 
-# ################
-# ## Tray Icons ##
-# ################
-
-# if ! command -v ckb-next &>/dev/null; then
-#   echo "ckb-next could not be found"
-# else
-#   if ! pgrep -x "ckb-next" >/dev/null; then
-#     ckb-next --background &
-#   else
-#     echo "ckb-next already Running"
-#   fi
-# fi
-
-# if command -v nm-applet &>/dev/null; then
-#   if ! pgrep -x "nm-applet" >/dev/null; then
-#     nm-applet &
-#   else
-#     echo "nm-applet already Running"
-#   fi
-# fi
-
-# if command -v blueman-applet &>/dev/null; then
-#   if ! pgrep -x "blueman-applet" >/dev/null; then
-#     blueman-applet &
-#   else
-#     echo "blueman-applet already Running"
-#   fi
-# fi
-
-# if command -v touchpad-indicator &>/dev/null; then
-#   if ! pgrep -f "touchpad-indicator" >/dev/null; then
-#     touchpad-indicator &
-#   else
-#     echo "touchpad-indicator already Running"
-#   fi
-# fi
-
-# if command -v numlockx &>/dev/null; then
-#   numlockx on &
-# fi
-
-# if command -v volumeicon &>/dev/null; then
-#   if ! pgrep -x "volumeicon" >/dev/null; then
-#     volumeicon &
-#   else
-#     echo "volumeicon already Running"
-#   fi
-# fi
-
 # ###############
 # ## Auto Lock ##
 # ###############
 
-if command -v xidlehook &>/dev/null; then
-  # shellcheck disable=SC2155
-  export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
-  if ! pgrep -x "xidlehook" >/dev/null; then
-    xidlehook \
-      --not-when-fullscreen \
-      --not-when-audio \
-      --timer 300 \
-      "xrandr --output $PRIMARY_DISPLAY --brightness .1" \
-      "xrandr --output $PRIMARY_DISPLAY --brightness 1" \
-      --timer 30 \
-      "xrandr --output $PRIMARY_DISPLAY --brightness 1; multimonitorlock -l -- --time-str=%I:%M:%S %p" \
-      "" \
-      --timer 3600 \
-      "systemctl suspend" \
-      ""
-  else
-    echo "xidlehook already Running"
-  fi
+if command -v xautolock &>/dev/null; then
+    # shellcheck disable=SC2155
+    if ! pgrep -x "xautolock" >/dev/null; then
+        (
+            sleep 2s
+            xautolock \
+                -time 15 -locker "multimonitorlock -l -- --time-str=%I:%M:%S %p" \
+                -detectsleep
+        ) &
+    else
+        echo "xautolock already Running"
+    fi
+else
+    echo "xautolock could not be found"
 fi
 
 # ################
