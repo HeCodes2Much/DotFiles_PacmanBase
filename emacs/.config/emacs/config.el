bf953b103c38817995609288ded522625e35871b
The-Repo-Club <wayne6324@gmail.com>
2023-08-28 16:58:59 +0100

    Update

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -427,7 +427,7 @@
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
   ;; Sets the default theme to load!!! 
-  (load-theme 'doom-one t)
+  (load-theme 'minimal-mistakes t)
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
   ;; Corrects (and improves) org-mode's native fontification.
