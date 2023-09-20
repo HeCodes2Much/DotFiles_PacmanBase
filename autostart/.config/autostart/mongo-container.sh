1ac6862320cba98c242cd18a44f49ce3920e6a79
The-Repo-Club <wayne6324@gmail.com>
2023-09-20 20:28:19 +0100

    Update

---
+++ autostart/.config/autostart/mongo-container.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#-*-coding:utf-8 -*-
+#Auto updated?
+#   Yes
+#File:
+#   mongo-container.sh
+#Author:
+#   The-Repo-Club [wayne6324@gmail.com]
+#Github:
+#   https://github.com/The-Repo-Club/
+#
+#Created:
+#   Wed 20 September 2023, 10:16:50 AM [GMT+1]
+#Modified:
+#   Wed 20 September 2023, 04:23:46 PM [GMT+1]
+#
+#Description:
+#   <Todo>
+#
+#Dependencies:
+#   <None>
+#
+
+docker start my-container-name
