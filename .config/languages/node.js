e51ee3834106ffb5d38a3322021da2905840145d
HeCodes2Much <wayne6324@gmail.com>
2024-08-20 19:52:37 +0100

    Added proctl config files

---
+++ .config/languages/node.js
@@ -0,0 +1,21 @@
+/*-*-coding:utf-8 -*-
+ *Auto updated?
+ *   Yes
+ *File :
+ *   {{PROJECT_NAME}}
+ *Author :
+ *   {{AUTHOR_NAME}} [{{AUTHOR_EMAIL}}]
+ *Github :
+ *   {{AUTHOR_GIT}}
+ *
+ *Created:
+ *   {{CURRENT_DATE}}
+ *Last edited:
+ *   {{CURRENT_DATE}}
+ *
+ *Description:
+ *   <Todo>
+ *
+ *Dependencies:
+ *   <None>
+**/
