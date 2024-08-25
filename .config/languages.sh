76ac783a8f4405c254b3cfff9bbc69767a5c2c19
HeCodes2Much <wayne6324@gmail.com>
2024-08-25 20:07:54 +0100

    fixed proctl files

--- .config/languages.sh
+++ .config/languages.sh
@@ -2,5 +2,5 @@
 export AUTHOR_NAME='HeCodes2Much'
 export AUTHOR_EMAIL='wayne6324@gmail.com'
 export AUTHOR_WEBSITE='https://063240.xyz/'
-export AUTHOR_GIT='http://github.com/HeCodes2Much'
+export AUTHOR_GIT='https://github.com/HeCodes2Much'
 export LANGUAGE_DIR='/home/wayne/.config/languages'
